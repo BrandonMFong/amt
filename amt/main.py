5c467ba404aa41593f4728131aed47b55b6a0d6a
BrandonMFong <fong.m.brandon97@gmail.com>
2021-03-13 08:54:20 -0800

    encoding the notes table in the script

--- amt/main.py
+++ amt/main.py
@@ -10,6 +10,7 @@ import wave
 from scipy.fftpack import fft
 import os.path
 from os import path
+import os
 import math
 from sys import platform
 import sys
@@ -30,7 +31,7 @@ class xAudioHandler:
     _wavFile = "input.wav"
 
     # Notes Table 
-    _notesTable = "notestable.csv"
+    # _notesTable = "notestable.csv"
 
     # FFT size
     _numFrames = 0
@@ -39,9 +40,10 @@ class xAudioHandler:
     # C0 to B0
     _frequencyRef = [16.35, 17.32, 18.35, 19.45, 20.60, 21.83, 23.12, 24.50, 25.96, 27.50, 29.14, 30.87]
 
-    # Dataframe columns for DFT
-    _frequency = "Frequency"
-    _magnitude = "Magnitude"
+    # Dataframe columns
+    _frequency  = "Frequency"   # for Notestable and dft
+    _magnitude  = "Magnitude"   # for dft
+    _notes      = "Notes"       # for notestable
 
     # Analysis methods 
     fbar = 0
@@ -74,13 +76,19 @@ class xAudioHandler:
                 okayToContinue = False 
                 print("Error in making wav file path")
 
-            # notes table
-            # This is the overhead I use to determine notes
-            # Kevin uses PCP
-            self._notesTable = basePath + fsSeparator + self._notesTable
-            if self._notesTable.__len__ == 0:
+            # # notes table
+            # # This is the overhead I use to determine notes
+            # # Kevin uses PCP
+            # self._notesTable = basePath + fsSeparator + self._notesTable
+            # if self._notesTable.__len__ == 0:
+            #     okayToContinue = False 
+            #     print("Error in making Notes Table file path")
+        
+        if okayToContinue:
+            self._notesTableData = self.GenerateNotesTable()
+            if self._notesTableData is None:
                 okayToContinue = False 
-                print("Error in making Notes Table file path")
+                print("Error in generating the notes table")
 
         # Bit file
         if okayToContinue:
@@ -227,23 +235,33 @@ class xAudioHandler:
         return results
 
     def FBar(self):
+        """
+        I call this FBar standing for Fong Bar 
+        This is the method I came up with in CES400
+        """
         # Get the max value of the magnitude
         # peakRowValue = spectrumData.loc[spectrumData['Mag'].idxmax()]
         peakRowValue = self._dft.loc[self._dft['Magnitude'].idxmax()]
         peakRowNoteFrequency = peakRowValue['Frequency']
 
         # Determine note
-        frequencyColumn = "Frequency"
-        noteColumn = "Note"
-        notesTableData = pd.read_csv("notestable.csv", header=None, names=[noteColumn, frequencyColumn])
-        freqArray = np.array(notesTableData[frequencyColumn])
+        # frequencyColumn = "Frequency"
+        # noteColumn = "Note"
+        # notesTableData = pd.read_csv(self._notesTable, header=None, names=[noteColumn, frequencyColumn])
+        freqArray = np.array(self._notesTableData[self._frequency])
         absFreqArray = np.abs(freqArray - peakRowNoteFrequency)
         smallestDiffIndex = absFreqArray.argmin()
         # freqCandidate = freqArray[smallestDiffIndex]
-        print("Note:", notesTableData.loc[smallestDiffIndex, noteColumn])
-        print("Frequency:", notesTableData.loc[smallestDiffIndex, frequencyColumn], "Hz")
+        print("Note:", self._notesTableData.loc[smallestDiffIndex, self._notes])
+        print("Frequency:", self._notesTableData.loc[smallestDiffIndex, self._frequency], "Hz")
+
+    def GenerateNotesTable(self):
+        notes = np.array(["C","C#/Db","D","D#/Eb","E","F","F#/Gb","G","G#/Ab","A","A#/Bb","B","C","C#/Db","D","D#/Eb","E","F","F#/Gb","G","G#/Ab","A","A#/Bb","B","C","C#/Db","D","D#/Eb","E","F","F#/Gb","G","G#/Ab","A","A#/Bb","B","C","C#/Db","D","D#/Eb","E","F","F#/Gb","G","G#/Ab","A","A#/Bb","B","C","C#/Db","D","D#/Eb","E","F","F#/Gb","G","G#/Ab","A","A#/Bb","B","C","C#/Db","D","D#/Eb","E","F","F#/Gb","G","G#/Ab","A","A#/Bb","B","C","C#/Db","D","D#/Eb","E","F","F#/Gb","G","G#/Ab","A","A#/Bb","B","C","C#/Db","D","D#/Eb","E","F","F#/Gb","G","G#/Ab","A","A#/Bb","B","C","C#/Db","D","D#/Eb","E","F","F#/Gb","G","G#/Ab","A","A#/Bb","B"])
+        freq = np.array([16.35,17.32,18.35,19.45,20.6,21.83,23.12,24.5,25.96,27.5,29.14,30.87,32.7,34.65,36.71,38.89,41.2,43.65,46.25,49,51.91,55,58.27,61.74,65.41,69.3,73.42,77.78,82.41,87.31,92.5,98,103.83,110,116.54,123.47,130.81,138.59,146.83,155.56,164.81,174.61,185,196,207.65,220,233.08,246.94,261.63,277.18,293.66,311.13,329.63,349.23,369.99,392,415.3,440,466.16,493.88,523.25,554.37,587.33,622.25,659.25,698.46,739.99,783.99,830.61,880,932.33,987.77,1046.5,1108.73,1174.66,1244.51,1318.51,1396.91,1479.98,1567.98,1661.22,1760,1864.66,1975.53,2093,2217.46,2349.32,2489.02,2637.02,2793.83,2959.96,3135.96,3322.44,3520,3729.31,3951.07,4186.01,4434.92,4698.63,4978.03,5274.04,5587.65,5919.91,6271.93,6644.88,7040,7458.62,7902.1])
+        result = pd.DataFrame({self._notes : notes, self._frequency : freq})
+        return result
 
 if __name__ == "__main__":
-    audioReader = xAudioHandler(baseBitFile=bitFile, inputPort="select_line_in")
+    audioReader = xAudioHandler(baseBitFile=bitFile, inputPort="select_line_in", analysisMethod=xAudioHandler.fbar)
     audioReader.run(recordInterval=0.5)
     # np.fft.rfft()
