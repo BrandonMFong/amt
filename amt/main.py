7bd85cd3274dc7f124d409ad373582e8cbc6e9c1
BrandonMFong <fong.m.brandon97@gmail.com>
2021-03-14 08:24:41 -0700

    Testing threshold limit and adding notes to code

--- amt/main.py
+++ amt/main.py
@@ -152,6 +152,8 @@ class xAudioHandler:
             # Analyze spectrum 
             self.analyze()
 
+            break
+
     def record(self, seconds):
         # Default state to 0.5 seconds
         if seconds is None:
@@ -210,6 +212,8 @@ class xAudioHandler:
 
         if okayToContinue:
             print("\nNotes:")
+
+            # Only print values on the vector that is not zero 
             print(pcpVector[pcpVector[self._result] != 0].loc[:, self._notes])
 
         if okayToContinue == False:
@@ -217,6 +221,9 @@ class xAudioHandler:
 
     # PCP
     def PCP(self):
+        """
+        This returns a list or a dictionary 
+        """
         results = {}
         baseNote = self._frequencyRef[0]
         threshold = 3000000
@@ -244,8 +251,11 @@ class xAudioHandler:
 
     def PCP2(self):
         """
-        I call this pcp2 standing for Fong Bar 
-        This is the method I came up with in CES400
+        PCP 2:
+            - Find all local maximum magnitudes
+            - iterate through those points 
+            - if the magnitude at those points are over a threshold, 
+                - YES: find the closes note value and record that into the pcp vector 
         """
         # Construct the pcp vector
         temp = np.zeros(self._noteLabels.size)
@@ -271,11 +281,13 @@ class xAudioHandler:
 
                 # Record the value in the pcp vector 
                 note = self._notesTableData.loc[smallestDiffIndex, self._notes]
-                # freq = self._notesTableData.loc[smallestDiffIndex, self._frequency]
                 results.loc[results[results[self._notes] == note][self._result].index, self._result] += 1
         return results
 
     def GenerateNotesTable(self):
+        """
+        Generates a dataframe of notes and their corresonding frequencies 
+        """
         notes = np.array(["C","C#/Db","D","D#/Eb","E","F","F#/Gb","G","G#/Ab","A","A#/Bb","B","C","C#/Db","D","D#/Eb","E","F","F#/Gb","G","G#/Ab","A","A#/Bb","B","C","C#/Db","D","D#/Eb","E","F","F#/Gb","G","G#/Ab","A","A#/Bb","B","C","C#/Db","D","D#/Eb","E","F","F#/Gb","G","G#/Ab","A","A#/Bb","B","C","C#/Db","D","D#/Eb","E","F","F#/Gb","G","G#/Ab","A","A#/Bb","B","C","C#/Db","D","D#/Eb","E","F","F#/Gb","G","G#/Ab","A","A#/Bb","B","C","C#/Db","D","D#/Eb","E","F","F#/Gb","G","G#/Ab","A","A#/Bb","B","C","C#/Db","D","D#/Eb","E","F","F#/Gb","G","G#/Ab","A","A#/Bb","B","C","C#/Db","D","D#/Eb","E","F","F#/Gb","G","G#/Ab","A","A#/Bb","B"])
         freq = np.array([16.35,17.32,18.35,19.45,20.6,21.83,23.12,24.5,25.96,27.5,29.14,30.87,32.7,34.65,36.71,38.89,41.2,43.65,46.25,49,51.91,55,58.27,61.74,65.41,69.3,73.42,77.78,82.41,87.31,92.5,98,103.83,110,116.54,123.47,130.81,138.59,146.83,155.56,164.81,174.61,185,196,207.65,220,233.08,246.94,261.63,277.18,293.66,311.13,329.63,349.23,369.99,392,415.3,440,466.16,493.88,523.25,554.37,587.33,622.25,659.25,698.46,739.99,783.99,830.61,880,932.33,987.77,1046.5,1108.73,1174.66,1244.51,1318.51,1396.91,1479.98,1567.98,1661.22,1760,1864.66,1975.53,2093,2217.46,2349.32,2489.02,2637.02,2793.83,2959.96,3135.96,3322.44,3520,3729.31,3951.07,4186.01,4434.92,4698.63,4978.03,5274.04,5587.65,5919.91,6271.93,6644.88,7040,7458.62,7902.1])
         result = pd.DataFrame({self._notes : notes, self._frequency : freq})
@@ -284,4 +296,3 @@ class xAudioHandler:
 if __name__ == "__main__":
     audioReader = xAudioHandler(baseBitFile=bitFile, inputPort="select_line_in", analysisMethod=xAudioHandler.pcp2)
     audioReader.run(recordInterval=0.5)
-    # np.fft.rfft()
