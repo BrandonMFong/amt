a987e3d205caa6a6b48c2c1ee31f7e03609c688a
BrandonMFong <fong.m.brandon97@gmail.com>
2021-03-21 19:39:44 -0700

    Reading of file

--- site/site/index.php
+++ site/site/index.php
@@ -3,6 +3,10 @@
         <meta http-equiv="refresh" content="5" />
     </head>
     <body>
-        <?php echo "The time is " . date("h:i:sa"); ?>
+        <?php 
+            echo "The time is " . date("h:i:sa"); 
+            $file = file_get_contents('./test.txt', FILE_USE_INCLUDE_PATH);
+            echo "<br/>" . $file;
+        ?>
     </body>
 </html>
