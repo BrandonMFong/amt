022ba400c785e4d6b5f7836a3691dc9553d246e6
BrandonMFong <fong.m.brandon97@gmail.com>
2021-03-25 18:49:20 -0700

    stable upload.php

--- site/site/upload.php
+++ site/site/upload.php
@@ -1,12 +1,27 @@
 <?php
-print_r($_FILES); //this will print out the received name, temp name, type, size, etc.
+try 
+{
+    // print_r($_FILES); //this will print out the received name, temp name, type, size, etc.
 
-$size = $_FILES['audio_data']['size']; //the size in bytes
-$input = $_FILES['audio_data']['tmp_name']; //temporary name that PHP gave to the uploaded file
-// $output = $_FILES['audio_data']['name'].".wav"; //letting the client control the filename is a rather bad idea
-$location = dirname(__FILE__) . "/";
-$output = $location . "input.wav";
-
-//move the file from temp name to local folder using $output name
-// move_uploaded_file($input, $output)
+    $okayToContinue = True;
+    $size = $_FILES['audio_data']['size']; //the size in bytes
+    $input = $_FILES['audio_data']['tmp_name']; //temporary name that PHP gave to the uploaded file
+    $output = $_FILES['audio_data']['name'].".wav"; //letting the client control the filename is a rather bad idea
+    
+    if (file_exists($input)) {
+        echo "The file $input exists\n";
+    } else {
+        echo "The file $input does not exist\n";
+    }
+    
+    //move the file from temp name to local folder using $output name
+    if(move_uploaded_file($input, $output)) {
+        echo "Copy Success";
+    } else {
+        // echo json_encode( [ 'message' => 'Copy failure' ]);
+        echo "Copy Failure";
+    }
+} catch (Exception $e) {
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
+}
 ?>
