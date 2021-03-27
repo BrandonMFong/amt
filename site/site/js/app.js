01fac0d7b8ec6a8c942fc4ea099e33c2fef050ae
BrandonMFong <fong.m.brandon97@gmail.com>
2021-03-27 10:07:59 -0700

    Continuous streaming

--- site/site/js/app.js
+++ site/site/js/app.js
@@ -21,74 +21,81 @@ pauseButton.addEventListener("click", pauseRecording);
 streamButton.addEventListener("click", streamRecording);
 
 streamButton.disabled = false;
-function streamRecording() {
+async function streamRecording() {
 	console.log("Stream clicked");
 
 	/*
 		Simple constraints object, for more advanced audio features see
 		https://addpipe.com/blog/audio-constraints-getusermedia/
 	*/
-    var constraints = { audio: true, video:false }
+	var constraints = { audio: true, video:false }
 
+	var stream = await navigator.mediaDevices.getUserMedia(constraints);
 
-	/*
-    	We're using the standard promise based getUserMedia() 
-    	https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
-	*/
-	navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-		console.log("getUserMedia() success, stream created, initializing Recorder.js ...");
-
-		/*
-			create an audio context after getUserMedia is called
-			sampleRate might change after getUserMedia is called, like it does on macOS when recording through AirPods
-			the sampleRate defaults to the one set in your OS for your playback device
-
-		*/
-		audioContext = new AudioContext();
-
-		//update the format 
-		document.getElementById("formats").innerHTML="Format: 1 channel pcm @ "+audioContext.sampleRate/1000+"kHz"
-
-		/*  assign to gumStream for later use  */
-		gumStream = stream;
-		
-		/* use the stream */
-		input = audioContext.createMediaStreamSource(stream);
-
-		/* 
-			Create the Recorder object and configure to record mono sound (1 channel)
-			Recording 2 channels  will double the file size
-		*/
-		rec = new Recorder(input,{numChannels:1})
-
-		//start the recording process
-		rec.record()
-		console.log("Recording started");
-
-		// Wait 2 seconds before stopping the recording 
-		sleep(2000).then(() => { 
-			rec.stop(); 
-			console.log("Recording stopped"); 
-			gumStream.getAudioTracks()[0].stop();
-			rec.exportWAV(function(blob){
-				console.log("Uploading audio to server");
-				var xhr=new XMLHttpRequest();
-				var filename = "temp.wav";
-				xhr.onload=function(e) {
-					if(this.readyState === 4) {
-						console.log("Server returned: ",e.target.responseText);
-					}
-				};
-				var fd=new FormData();
-				fd.append("audio_data",blob, filename);
-				xhr.open("POST","upload.php",true);
-				xhr.send(fd);
-			});
-		});
+	while(1) {
 	
-	}).catch(function(err) {
-		console.log("Error in streaming");
-	});
+		/*
+			We're using the standard promise based getUserMedia() 
+			https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+		*/
+		// navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+		// stream.then(function(stream) {
+			console.log("getUserMedia() success, stream created, initializing Recorder.js ...");
+	
+			/*
+				create an audio context after getUserMedia is called
+				sampleRate might change after getUserMedia is called, like it does on macOS when recording through AirPods
+				the sampleRate defaults to the one set in your OS for your playback device
+	
+			*/
+			audioContext = new AudioContext();
+	
+			//update the format 
+			document.getElementById("formats").innerHTML="Format: 1 channel pcm @ "+audioContext.sampleRate/1000+"kHz"
+	
+			/*  assign to gumStream for later use  */
+			gumStream = stream;
+			
+			/* use the stream */
+			input = audioContext.createMediaStreamSource(stream);
+	
+			/* 
+				Create the Recorder object and configure to record mono sound (1 channel)
+				Recording 2 channels  will double the file size
+			*/
+			rec = new Recorder(input,{numChannels:1})
+	
+			//start the recording process
+			rec.record()
+			console.log("Recording started");
+	
+			// Wait 2 seconds before stopping the recording 
+			// await sleep(2000).then(() => { 
+			await sleep(2000).then(async function () { 
+				rec.stop(); 
+				console.log("Recording stopped"); 
+				gumStream.getAudioTracks()[0].stop();
+				rec.exportWAV(function(blob){
+					console.log("Uploading audio to server");
+					var xhr=new XMLHttpRequest();
+					var filename = "temp.wav";
+					xhr.onload=function(e) {
+						if(this.readyState === 4) {
+							console.log("Server returned: ",e.target.responseText);
+						}
+					};
+					var fd=new FormData();
+					fd.append("audio_data",blob, filename);
+					xhr.open("POST","upload.php",true);
+					xhr.send(fd);
+				});
+			});
+		
+		// }).catch(function(err) {
+		// 	console.log("Error in streaming");
+		// });
+	}
+
 }
 function sleep(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms));
