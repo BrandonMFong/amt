e9beca3220894c7860a0c8c20fc7aa8633c259ef
BrandonMFong <fong.m.brandon97@gmail.com>
2021-05-17 19:52:21 -0700

    Adding adders

---
+++ Vivado/IP/adder/adder/solution1/sdx.tcl
@@ -0,0 +1,10 @@
+# ==============================================================
+# File generated on Mon May 17 19:45:54 -0700 2021
+# Vivado(TM) HLS - High-Level Synthesis from C, C++ and SystemC v2018.3 (64-bit)
+# SW Build 2405991 on Thu Dec  6 23:38:27 MST 2018
+# IP Build 2404404 on Fri Dec  7 01:43:56 MST 2018
+# Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+# ==============================================================
+add_files adder.cpp
+set_part xc7z020clg400-1
+create_clock -name default -period 10
---
+++ Vivado/IP/adder/adder/vivado_hls.app
@@ -0,0 +1,14 @@
+<project xmlns="com.autoesl.autopilot.project" top="add" name="adder">
+    <includePaths/>
+    <libraryPaths/>
+    <Simulation>
+        <SimFlow name="csim"/>
+    </Simulation>
+    <files xmlns="">
+        <file name="adder.cpp" sc="0" tb="false" cflags="" blackbox="false"/>
+    </files>
+    <solutions xmlns="">
+        <solution name="solution1" status="active"/>
+    </solutions>
+</project>
+
