cb4317912ae5836cda9d40c876a9c69d4099e329
BrandonMFong <fong.m.brandon97@gmail.com>
2021-06-06 10:10:33 -0700

    Data can be passed through

--- Vivado/rev2/myBD/myBD.srcs/sources_1/bd/design_1/ip/design_1_myIP_0_0/design_1_myIP_0_0_stub.v
+++ Vivado/rev2/myBD/myBD.srcs/sources_1/bd/design_1/ip/design_1_myIP_0_0/design_1_myIP_0_0_stub.v
@@ -1,7 +1,7 @@
 // Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2018.3 (win64) Build 2405991 Thu Dec  6 23:38:27 MST 2018
-// Date        : Sat Jun  5 09:33:39 2021
+// Date        : Sun Jun  6 09:38:16 2021
 // Host        : KAMANTA running 64-bit major release  (build 9200)
 // Command     : write_verilog -force -mode synth_stub
 //               B:/COLLEGE/Thesis/Source/Vivado/rev2/myBD/myBD.srcs/sources_1/bd/design_1/ip/design_1_myIP_0_0/design_1_myIP_0_0_stub.v
--- Vivado/rev2/myBD/myBD.srcs/sources_1/bd/design_1/ip/design_1_myIP_0_0/sim/design_1_myIP_0_0.v
+++ Vivado/rev2/myBD/myBD.srcs/sources_1/bd/design_1/ip/design_1_myIP_0_0/sim/design_1_myIP_0_0.v
@@ -48,7 +48,7 @@
 
 
 // IP VLNV: xilinx.com:user:myIP:1.1
-// IP Revision: 11
+// IP Revision: 12
 
 `timescale 1ns/1ps
 
