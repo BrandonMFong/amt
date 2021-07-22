153a1481333a33c6e670e380ba5498864624b0b2
BrandonMFong <fong.m.brandon97@gmail.com>
2021-07-21 18:51:23 -0700

    Connecting modules

--- Vivado/rev4/PCPIP/PCPIP.srcs/sources_1/imports/new/PCP.v
+++ Vivado/rev4/PCPIP/PCPIP.srcs/sources_1/imports/new/PCP.v
@@ -41,6 +41,11 @@ module PCP #(
     input wire [C_AXIS_TDATA_WIDTH+2-1:0] inputValue,
     
     /**
+    *   If input is starting to stream data
+    */
+    input wire inputValid,
+    
+    /**
     *   If master axi interface is ready to start transferring data
     */
     input wire axiReady,
@@ -49,12 +54,6 @@ module PCP #(
     *   Initiates a reset. This shoud be driven by the axi slave's reset
     */
     input wire reset, 
-    
-    /**
-    *   If we can start readying input.  If true, the inputValue will be expected to
-    *   change every clock period
-    */
-    input wire readyFlag,
     
     /* OUTPUT */
     
@@ -68,66 +67,23 @@ module PCP #(
     */
     output wire outputValid
 );
-//    /* LOCAL PARAMETERS */
-//    localparam kCounterWidth = 32;
-//    localparam kMaxWaitCount = 2**kCounterWidth;
-//    localparam kTotalNumberOfStates = 2;
 
-//    /* REGISTERS */
-//    reg [C_AXIS_TDATA_WIDTH - 1 : 0] pcpVector [2**ADDR_WIDTH - 1 : 0]; // PCP vector
-//    reg validOutputData;                                            // If value at outputValue is valid
-//    reg lastDataFlag = 1'b0;                                        // Indicates the last data value in output stream
-//    reg [C_AXIS_TDATA_WIDTH-1:0] outData;                           // Outbound data
-//    reg pcpReady = 1'b0;                                            // If PCP is ready to write to output
-//    reg [2**PCP_ADDR_WIDTH - 1 : 0]    inAddr,            // Address for writing
-//                            outAddr;                          // Address for reading
-//    reg [C_AXIS_TDATA_WIDTH - 1 : 0] magnitudeValueBuffer;
-//    reg [kCounterWidth - 1 : 0] counter;
-                        
+    wire [C_AXIS_TDATA_WIDTH - 1 : 0]   dataStream;
+    wire                                lastDataFlag;
+    wire                                recordPCPValue; // If Datastream has finished parsing current data
+    wire [PCP_ADDR_WIDTH - 1 : 0]       profileNumber;
+    wire [C_AXIS_TDATA_WIDTH-1:0]       magnitudeOutput;
     
-//    /* WIRES */
-//    wire writeOutput; // 1 if we are writing to outputValue
-//    wire recordPCP;
-//    wire [C_AXIS_TDATA_WIDTH - 1 : 0] magnitudeValue;
-//    wire [2**PCP_ADDR_WIDTH - 1 : 0] pcpNumber; // Used also as addr
-    
-//    /* INITIALIZATION */
-//    initial begin 
-//        lastDataFlag    = 1'b0;
-//        outData         = {C_AXIS_TDATA_WIDTH{1'b0}};
-//        pcpReady        = 1'b0;
-//        inAddr          = {ADDR_WIDTH{1'b0}};
-//        outAddr         = {ADDR_WIDTH{1'b0}};
-//        validOutputData = 1'b0;
-//        magnitudeValueBuffer = {C_AXIS_TDATA_WIDTH{1'b0}};
-//        counter = 32'b0;
-//    end 
-    
-//    /* COMBINATION LOGIC */
-    
-//    /* BEHAVIORAL LOGIC */
-    
-//    /**
-//    *   Parse the data stream and output the pcp value
-//    */
-//    DataStream #(
-//        .ADDR_WIDTH(ADDR_WIDTH),
-//        .C_AXIS_TDATA_WIDTH(C_AXIS_TDATA_WIDTH),
-//        .OUTPUT_DATA_WIDTH(PCP_ADDR_WIDTH)
-//    ) mod0 (
-//        .clk(clk),
-//        .inputStream(inputValue),
-//        .startReading(readyFlag),
-//        .ready(recordPCP),
-//        .profileNumber(pcpNumber),
-//        .magnitudeValue(magnitudeValue)
-//    );
+    DataStream mod0 (
+        .clk            (clk),
+        .inputStream    (dataStream),
+        .startReading   (inputValid),
+        .ready          (recordPCPValue),
+        .profileNumber  (profileNumber),
+        .magnitudeValue (magnitudeOutput)
+    );
 
-//    /* ASSIGNMENTS */
-//    assign outputValue  = {lastDataFlag, outData};
-//    assign writeOutput  = axiReady & pcpReady;
-//    assign outputValid  = validOutputData;
-
+    assign {lastDataFlag, dataStream} = inputValue;
     assign outputValue = inputValue;
     
 endmodule
