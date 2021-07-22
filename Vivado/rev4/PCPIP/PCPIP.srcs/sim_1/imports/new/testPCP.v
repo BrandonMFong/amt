7d427d5b294adb0a5dda07d468063edda2e07e02
BrandonMFong <fong.m.brandon97@gmail.com>
2021-07-21 18:32:45 -0700

    Tests for datastream and profiler passes

---
+++ Vivado/rev4/PCPIP/PCPIP.srcs/sim_1/imports/new/testPCP.v
@@ -0,0 +1,92 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/21/2021 06:02:42 PM
+// Design Name: 
+// Module Name: testPCP
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module testPCP;
+
+    localparam kMaxCount = 30;
+    
+    reg ready;
+    reg clk;
+    reg reset;
+    reg readyFlag;
+    reg [31 : 0] counter;
+    wire [15 : 0] outputValue;
+    wire outputValid;
+    
+    PCP UUT0 (
+        .clk            (clk), 
+        .axiReady       (ready), 
+        .outputValue    (outputValue),
+        .reset          (reset),
+        .outputValid    (outputValid),
+        .readyFlag      (readyFlag)
+    );
+    
+    initial begin // initial block executes only once
+        reset = 1;
+        clk = 1;
+        ready = 0;
+        readyFlag = 0;
+        
+        #5
+        clk = ~clk;
+        #5
+        clk = ~clk;
+        #5
+        clk = ~clk;
+        #5
+        clk = ~clk;
+        #5
+        clk = ~clk;
+        #5
+        clk = ~clk;
+        #5
+        clk = ~clk;
+        #5
+        clk = ~clk;
+        #5
+        clk = ~clk;
+        
+        reset = 1;
+        forever begin
+            #5
+            clk = ~clk;
+            ready = 1;
+            
+            // At positive edge of clk
+            if (clk) begin 
+                readyFlag = ~readyFlag;
+                // Reset the reset 
+                if (reset) begin 
+                    reset = 1'b0;
+                end 
+                
+                if (counter < kMaxCount) begin 
+                    counter = counter + 1;
+                    reset = 1'b0;
+                end else begin 
+                    counter = {31{1'b0}};
+                    reset = 1'b1;
+                end 
+            end
+        end
+    end
+endmodule
