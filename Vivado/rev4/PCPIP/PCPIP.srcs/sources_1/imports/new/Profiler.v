34b649a1480bcfdef421a92e0aff613ba5f0dcc8
BrandonMFong <fong.m.brandon97@gmail.com>
2021-07-24 10:05:27 -0700

    Can synthesize

--- Vivado/rev4/PCPIP/PCPIP.srcs/sources_1/imports/new/Profiler.v
+++ Vivado/rev4/PCPIP/PCPIP.srcs/sources_1/imports/new/Profiler.v
@@ -35,7 +35,7 @@ module Profiler #(
 )(
 
     /**
-    *   Holds frequency value
+    *   Holds frequency value from 0 to SAMPLING_RATE 
     */
     input wire [C_AXIS_TDATA_WIDTH - 1 : 0] frequencyValue,
     
@@ -43,43 +43,30 @@ module Profiler #(
     * Output value has to be between 0 to 11.  We need 
     * 4 bits to represent that number
     */
-    output wire [2**OUTPUT_DATA_WIDTH - 1 : 0] outputValue
+    output wire [OUTPUT_DATA_WIDTH - 1 : 0] outputValue
 );
-    localparam k32BitWidth = 32;
-    
-    integer                         intFrequencyValue;
-    reg [C_AXIS_TDATA_WIDTH - 1 : 0] logResult;
-    integer                         roundedLogResult; // I don't need more than 255
-    reg [k32BitWidth - 1 : 0]       logRegResult;
-    reg [2**OUTPUT_DATA_WIDTH - 1 : 0] outputBuffer;
     integer i;
-    reg [10 : 0] log2x12LUT [0 : (2**$clog2(SAMPLING_RATE)) - 1];
+    real logResultReal;
+    integer logResultInt;
+    reg [OUTPUT_DATA_WIDTH - 1 : 0] outputBuffer;
+    reg [OUTPUT_DATA_WIDTH - 1 : 0] pcpNumLUT [0 : SAMPLING_RATE - 1];
     
     initial begin 
-        intFrequencyValue       = 0;
-        logResult               = 0.0;
-        roundedLogResult        = 0;
-        logRegResult            = {k32BitWidth{1'b0}};
-        outputBuffer            = {2**OUTPUT_DATA_WIDTH{1'b0}};
+        outputBuffer = {2**OUTPUT_DATA_WIDTH{1'b0}};
+        
+        pcpNumLUT[0] = {OUTPUT_DATA_WIDTH{1'b0}};
         
         // initialize LUT
         // Maximum value would be SAMPLING_RATE
-        for (i = 0; i < SAMPLING_RATE; i = i + 1) begin 
-            log2x12LUT[i] = 12 * ($log10(i) / $log10(2));
+        for (i = 1; i < SAMPLING_RATE; i = i + 1) begin 
+            logResultReal = $log10(i) / $log10(2);
+            logResultReal = 12 * logResultReal;
+            logResultInt = logResultReal;
+            logResultInt = logResultInt % 12;
+            pcpNumLUT[i] = logResultInt;
         end
     end 
-
-//    /**
-//    *   Should calculate the pcp vector 
-//    */
-//    always @(*) begin 
-//        intFrequencyValue       = frequencyValue;
-//        logResult               = log2LUT[intFrequencyValue];
-//        roundedLogResult        = logResult; // float to int which will round the value
-//        logRegResult            = roundedLogResult;
-//        outputBuffer            = logRegResult % 12;
-//    end 
     
-    assign outputValue = outputBuffer;
+    assign outputValue = pcpNumLUT[frequencyValue];
 
 endmodule
