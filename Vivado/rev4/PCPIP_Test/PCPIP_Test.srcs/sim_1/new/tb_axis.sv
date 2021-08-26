dca76d78b32c9e9ac1068ef74feefee19b2c2633
BrandonMFong <fong.m.brandon97@gmail.com>
2021-08-25 17:59:56 -0700

    Can sim

---
+++ Vivado/rev4/PCPIP_Test/PCPIP_Test.srcs/sim_1/new/tb_axis.sv
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/24/2021 05:49:03 PM
+// Design Name: 
+// Module Name: tb_axis
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+import axi4stream_vip_pkg::*;
+import design_1_axi4stream_vip_0_0_pkg::*;
+import design_1_axi4stream_vip_1_0_pkg::*;
+
+module tb_axis(
+
+    );
+endmodule
