2b89ef8083795977eff80b43e5f9bc91d1260b6a
BrandonMFong <fong.m.brandon97@gmail.com>
2021-11-28 08:34:08 -0800

    Good build

--- Vivado/rev4/pcp_design/pcp_design.srcs/sources_1/bd/pcp_design/ip/pcp_design_axis_fifo_v1_0_0_0/pcp_design_axis_fifo_v1_0_0_0_sim_netlist.v
+++ Vivado/rev4/pcp_design/pcp_design.srcs/sources_1/bd/pcp_design/ip/pcp_design_axis_fifo_v1_0_0_0/pcp_design_axis_fifo_v1_0_0_0_sim_netlist.v
@@ -1,7 +1,7 @@
 // Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2018.3 (win64) Build 2405991 Thu Dec  6 23:38:27 MST 2018
-// Date        : Tue Sep 21 20:17:27 2021
+// Date        : Sun Nov 28 08:12:36 2021
 // Host        : KAMANTA running 64-bit major release  (build 9200)
 // Command     : write_verilog -force -mode funcsim
 //               B:/COLLEGE/Thesis/Source/Vivado/rev4/pcp_design/pcp_design.srcs/sources_1/bd/pcp_design/ip/pcp_design_axis_fifo_v1_0_0_0/pcp_design_axis_fifo_v1_0_0_0_sim_netlist.v
--- Vivado/rev4/pcp_design/pcp_design.srcs/sources_1/bd/pcp_design/ip/pcp_design_axis_fifo_v1_0_0_0/synth/pcp_design_axis_fifo_v1_0_0_0.v
+++ Vivado/rev4/pcp_design/pcp_design.srcs/sources_1/bd/pcp_design/ip/pcp_design_axis_fifo_v1_0_0_0/synth/pcp_design_axis_fifo_v1_0_0_0.v
@@ -48,11 +48,11 @@
 
 
 // IP VLNV: xilinx.com:user:axis_fifo_v1_0:1.0
-// IP Revision: 54
+// IP Revision: 55
 
 (* X_CORE_INFO = "axis_fifo_v1_0,Vivado 2018.3" *)
 (* CHECK_LICENSE_TYPE = "pcp_design_axis_fifo_v1_0_0_0,axis_fifo_v1_0,{}" *)
-(* CORE_GENERATION_INFO = "pcp_design_axis_fifo_v1_0_0_0,axis_fifo_v1_0,{x_ipProduct=Vivado 2018.3,x_ipVendor=xilinx.com,x_ipLibrary=user,x_ipName=axis_fifo_v1_0,x_ipVersion=1.0,x_ipCoreRevision=54,x_ipLanguage=VERILOG,x_ipSimLanguage=MIXED,ADDR_WIDTH=32,C_AXIS_TDATA_WIDTH=64}" *)
+(* CORE_GENERATION_INFO = "pcp_design_axis_fifo_v1_0_0_0,axis_fifo_v1_0,{x_ipProduct=Vivado 2018.3,x_ipVendor=xilinx.com,x_ipLibrary=user,x_ipName=axis_fifo_v1_0,x_ipVersion=1.0,x_ipCoreRevision=55,x_ipLanguage=VERILOG,x_ipSimLanguage=MIXED,ADDR_WIDTH=32,C_AXIS_TDATA_WIDTH=64}" *)
 (* IP_DEFINITION_SOURCE = "package_project" *)
 (* DowngradeIPIdentifiedWarnings = "yes" *)
 module pcp_design_axis_fifo_v1_0_0_0 (
